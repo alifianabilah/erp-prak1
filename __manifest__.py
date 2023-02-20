2e36e09a88fdac23069ae8bced6d185022b290a4
Alifia Nabilah <alifianabilahh@gmail.com>
2023-02-20 14:54:43 +0700

    erp-prak1

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+{
+"name"          : "New Addons",
+"version"       : "11.0",
+"author"        : "Alifia Shofa Nabilah",
+"website"       : "https://www.detik.com/",
+"category"      : "New Modul",
+"summary"       : "Ini adalah addons baru",
+"description"   : """
+        Latian membuat addons baru di odoo 1
+""",
+"depends"       : [
+    "base",
+],
+"data"          : [
+    
+],
+"demo"          :[],
+"test"          :[],
+"image"         :[],
+"qweb"          :[],
+"css"           :[],
+"application"   :[],
+"installable"   :[],
+"auto_install"  :[],
+}
